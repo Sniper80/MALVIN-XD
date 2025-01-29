713e95c19651367cbf0247c155abc0752f285dbe
Sniper80 <75849429+Sniper80@users.noreply.github.com>
2025-01-30 01:39:18 +0200

    Update del.js

--- malvin/plugins/del.js
+++ malvin/plugins/del.js
@@ -23,6 +23,6 @@ const key = {
         await conn.sendMessage(m.chat, { delete: key })
 } catch(e) {
 console.log(e);
-reply('✦ᴍᴀʟᴠɪɴ xᴅ✦ successful..👨‍💻✅')
+reply('✦𝚂𝙽𝙸𝙿𝙴𝚁✦ successful..👨‍💻✅')
 } 
 })
